dc81225abbc254b336fd39667c4e4d50c5950f4a
flovis <floviskabandafkm@gmail.com>
2024-04-22 16:35:29 +0200

    add on board page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,50 @@
-import { Link } from "expo-router";
+import { router, Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">Hello</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="items-center justify-center min-h-[84vh] w-full px-4">
+          <Image
+            source={images.logo}
+            className="w-32 h-32"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibility with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="absolute w-32 h-4 -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-center mt-7 text-gray-100">
+            Where creativity meets innovation: embark on a journey of limitness
+            exploration with Aora.
+          </Text>
 
-      <Link href="./home">Go to profile</Link>
-    </View>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-up")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor="#161622" />
+    </SafeAreaView>
   );
 }
 
---
+++ components/CustomButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyle,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyle}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CustomButton;
